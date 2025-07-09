991e03f5024a343cce906533895e6cd01a005fea
bonifacegrengbo <grengboboniface19@gmail.com>
2025-07-09 02:05:25 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.0"
 }
 
+provider "aws" {
+ region = var.aws_region
+}
+
 variable "aws_region" {
   description = "AWS region to deploy into"
   type        = string
